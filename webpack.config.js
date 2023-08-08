68bc350917ffd8c389d9a4fe60d5fa217dab96a0
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-08 18:46:52 +0300

    1.2.2 Создает webpack.config.js и конфиг. опции

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+};
