4a5c3fd24df6679a56722459615fe5cbb7054de1
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-09 14:36:15 +0300

    Добавляет сортировку

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import NewFilterView from './view/filtersView.js';
+import NewSortView from './view/sortView.js';
 import {render} from './render.js';
 
 const filtersElement = document.querySelector('.trip-controls__filters');
+const sortElement = document.querySelector('.trip-events');
 
 
 render(new NewFilterView(), filtersElement);
+render(new NewSortView(), sortElement);
