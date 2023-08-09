e1e1666dd353c11116ddc415ad8aaa95d0d2a833
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-09 18:48:50 +0300

    Добавляет фильтры точек маршрута

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import './views/brief-view.js';
+import './views/filter-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
  */
 
+/**
+ * @type {import('./views/filter-view').default}
+ */
+
 const briefView = document.querySelector('brief-view');
+const filterView = document.querySelector('filter-view');
 
 briefView.render();
+filterView.render();
+
