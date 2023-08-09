8e91794256d637e608d0137ba10d6d7c5eebbb0e
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-09 17:18:55 +0300

    Добавляем сниппеты для view

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-import NewFilterView from './view/filtersView.js';
-import NewSortView from './view/sortView.js';
-import {render} from './render.js';
 
-const filtersElement = document.querySelector('.trip-controls__filters');
-const sortElement = document.querySelector('.trip-events');
-
-
-render(new NewFilterView(), filtersElement);
-render(new NewSortView(), sortElement);
