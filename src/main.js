7d8cba68c78205fe3649f0949552bbb0773b324b
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-18 18:53:00 +0300

    Добавляет FilterView

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import BriefViewView from './view/brief-view.js';
+import FilterView from './view/filter-view.js';
 import {RenderPosition, render} from './render.js';
 
 const tripMainElement = document.querySelector('.trip-main');
-
+const tripFiltersElement = document.querySelector('.trip-controls__filters');
 
 render(new BriefViewView(), tripMainElement, RenderPosition.AFTERBEGIN);
+render(new FilterView(), tripFiltersElement);
