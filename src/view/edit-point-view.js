22bff09732a1e35607b545d7c20adbd9becbc4b6
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-28 16:50:09 +0300

    Разбивает PointView и EditPointView на функции

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -36,93 +36,220 @@ export default class PointEditView {
 }
 
 function createEditPointViewTemplate(point, destinations, offers) {
-
   const {basePrice, dateFrom, dateTo, destinationId, offerIds, type} = point;
 
-  return /*html*/`<li class="trip-events__item">
-            <form class="event event--edit" action="#" method="post">
-              <header class="event__header">
-                <div class="event__type-wrapper">
-                  <label class="event__type  event__type-btn" for="event-type-toggle-1">
-                    <span class="visually-hidden">Choose event type</span>
-                    <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
-                  </label>
-                  <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-
-                  <div class="event__type-list">
-                    <fieldset class="event__type-group">
-                      <legend class="visually-hidden">Event type</legend>
-                      ${getSelectedOffers(type, offers)}
-                    </fieldset>
-                  </div>
-                </div>
-
-                <div class="event__field-group  event__field-group--destination">
-                  <label class="event__label  event__type-output" for="event-destination-1">
-                    ${point.type}
-                  </label>
-                  <input class="event__input  event__input--destination"
-                    id="event-destination-1" type="text" name="event-destination"
-                    value=${destinations.find((destination) => destination.id === destinationId).name}
-                    list="destination-list-1">
-                  <datalist id="destination-list-1">
-                    ${getDestinaniosString(destinations)}
-                  </datalist>
-                </div>
-
-                <div class="event__field-group  event__field-group--time">
-                  <label
-                    class="visually-hidden"
-                    for="event-start-time-1">
-                    From
-                    </label>
-                  <input class="event__input  event__input--time"
-                    id="event-start-time-1" type="text" name="event-start-time"
-                    value=${dateFrom}>
-                  —
-                  <label class="visually-hidden" for="event-end-time-1">To</label>
-                  <input class="event__input  event__input--time"
-                    id="event-end-time-1" type="text" name="event-end-time"
-                    value=${dateTo}>
-                </div>
-
-                <div class="event__field-group  event__field-group--price">
-                  <label class="event__label" for="event-price-1">
-                    <span class="visually-hidden">Price</span>
-                    €
-                  </label>
-                  <input class="event__input  event__input--price"
-                    id="event-price-1" type="text" name="event-price"
-                    value=${basePrice}>
-                </div>
-
-                <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-                <button class="event__reset-btn" type="reset">Delete</button>
-                <button class="event__rollup-btn" type="button">
-                  <span class="visually-hidden">Open event</span>
-                </button>
-              </header>
-              <section class="event__details">
-                <section class="event__section  event__section--offers">
-                  <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-                  <div class="event__available-offers">
-                    ${getOffers(type, offerIds, offers)}
-                  </div>
-                </section>
-                <section class="event__section  event__section--destination">
-                  ${getDestination(destinationId, destinations)}
-                </section>
-              </section>
-            </form>
-          </li>`;
+  return /*html*/`
+    <li class="trip-events__item">
+      <form class="event event--edit" action="#" method="post">
+        <header class="event__header">
+          ${getEventWrapper(type, offers)}
+          ${getDestinations(type, destinationId, destinations)}
+          ${getEventTime(dateFrom, dateTo)}
+          ${getBasePrice(basePrice)}
+          ${getSaveButton()}
+          ${getDeleteButton()}
+          ${getEventRollupButton()}
+        </header>
+        <section class="event__details">
+          ${getOffersList(type, offerIds, offers)}
+          ${getDestinationDescription(destinationId, destinations)}
+        </section>
+      </form>
+   </li>`;
 }
 
-function getDestinaniosString(destinations){
+function getEventWrapper(type, offers){
+  return /*html*/`
+    <div class="event__type-wrapper">
+      <label
+        class="event__type  event__type-btn"
+        for="event-type-toggle-1">
+        <span
+          class="visually-hidden">
+          Choose event type
+        </span>
+        <img
+          class="event__type-icon"
+          width="17"
+          height="17"
+          src="img/icons/${type}.png"
+          alt="Event type icon">
+      </label>
+      <input
+        class="event__type-toggle  visually-hidden"
+        id="event-type-toggle-1"
+        type="checkbox">
+      <div
+        class="event__type-list">
+        <fieldset
+          class="event__type-group">
+          <legend
+            class="visually-hidden">
+            Event type
+          </legend>
+          ${getSelectedOffer(type, offers)}
+        </fieldset>
+      </div>
+    </div>
+  `;
+}
+
+function getSelectedOffer(type, offers){
+  const selectedOffers = offers.map((offer) => /*html*/ `
+    <div
+      class="event__type-item">
+      <input
+        id="event-type-${offer.type}-1"
+        class="event__type-input  visually-hidden"
+        type="radio"
+        name="event-type"
+        value="${offer.type}"
+        ${type === offer.type ? 'checked' : ''}>
+      <label
+        class="event__type-label  event__type-label--${offer.type}"
+        for="event-type-${offer.type}-1">
+        ${offer.type[0].toUpperCase() + offer.type.slice(1)}
+      </label>
+    </div>`);
+
+  return selectedOffers.join('');
+}
+
+function getDestinations(type, destinationId, destinations){
+  return /*html*/`
+  <div
+    class="event__field-group  event__field-group--destination">
+    <label
+      class="event__label  event__type-output"
+      for="event-destination-1">
+      ${type}
+    </label>
+    <input
+      class="event__input  event__input--destination"
+      id="event-destination-1"
+      type="text"
+      name="event-destination"
+      value=${destinations.find((destination) => destination.id === destinationId).name}
+      list="destination-list-1">
+    <datalist
+      id="destination-list-1">
+      ${getDestinationsList(destinations)}
+    </datalist>
+  </div>
+  `;
+}
+
+function getDestinationsList(destinations){
   const destinationNames = destinations.map((destination) =>/*html*/`
   <option value="${destination.name}"></option>
   `);
 
   return destinationNames.join('');
+}
+
+function getEventTime(dateFrom, dateTo){
+  return /*html*/`
+    <div class="event__field-group  event__field-group--time">
+      <label
+        class="visually-hidden"
+        for="event-start-time-1">
+        From
+      </label>
+      <input
+        class="event__input  event__input--time"
+        id="event-start-time-1"
+        type="text"
+        name="event-start-time"
+        value=${dateFrom}>
+        —
+      <label
+        class="visually-hidden"
+        for="event-end-time-1">
+        To
+      </label>
+      <input
+        class="event__input  event__input--time"
+        id="event-end-time-1"
+        type="text"
+        name="event-end-time"
+        value=${dateTo}>
+    </div>
+  `;
+}
+
+function getBasePrice(basePrice){
+  return /*html*/`
+    <div class="event__field-group  event__field-group--price">
+      <label class="event__label" for="event-price-1">
+        <span class="visually-hidden">Price</span>
+        €
+      </label>
+      <input class="event__input  event__input--price"
+        id="event-price-1" type="text" name="event-price"
+        value=${basePrice}>
+      </div>
+  `;
+}
+
+function getSaveButton(){
+  return /*html*/`
+    <button
+      class="event__save-btn  btn  btn--blue"
+      type="submit">
+      Save
+    </button>
+  `;
+}
+
+function getDeleteButton(){
+  return /*html*/`
+    <button class="event__reset-btn" type="reset">Delete</button>
+  `;
+}
+
+function getEventRollupButton(){
+  return /*html*/`
+    <button class="event__rollup-btn" type="button">
+      <span class="visually-hidden">Open event</span>
+    </button>
+  `;
+}
+
+function getOffersList(type, offerIds, offers){
+  return /*html*/`
+    <section class="event__section  event__section--offers">
+      <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+      <div class="event__available-offers">
+        ${getOffers(type, offerIds, offers)}
+      </div>
+    </section>
+  `;
+}
+
+function getOffers(pointType, offerIds, offers){
+  const offersGroup = offers.find((offer) => pointType === offer.type);
+
+  const offersStrings = offersGroup?.offers.map((offer) => /*html*/`
+  <div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden"
+      id="event-offer-${offer.title}-1" type="checkbox" name="event-offer-${offer.title}"
+      ${offerIds.includes(offer.id) ? 'checked' : ''}>
+    <label class="event__offer-label" for="event-offer-${offer.title}-1">
+      <span class="event__offer-title">${offer.title}</span>
+      +€&nbsp;
+      <span class="event__offer-price">${offer.price}</span>
+    </label>
+  </div>`);
+
+  return offersStrings.join('');
+}
+
+function getDestinationDescription(destinationId, destinations){
+  return /*html*/`
+  <section class="event__section  event__section--destination">
+    ${getDestination(destinationId, destinations)}
+  </section>
+  `;
 }
 
 function getDestination(destinationId, destinations){
@@ -150,44 +277,4 @@ function getDestination(destinationId, destinations){
 
   return destinationString + photosString;
 }
-
-function getOffers(pointType, offerIds, offers){
-  const offersGroup = offers.find((offer) => pointType === offer.type);
-
-  const offersStrings = offersGroup?.offers.map((offer) => /*html*/`
-  <div class="event__offer-selector">
-    <input class="event__offer-checkbox  visually-hidden"
-      id="event-offer-${offer.title}-1" type="checkbox" name="event-offer-${offer.title}"
-      ${offerIds.includes(offer.id) ? 'checked' : ''}>
-    <label class="event__offer-label" for="event-offer-${offer.title}-1">
-      <span class="event__offer-title">${offer.title}</span>
-      +€&nbsp;
-      <span class="event__offer-price">${offer.price}</span>
-    </label>
-  </div>`);
-
-  return offersStrings.join('');
-}
-
-function getSelectedOffers(type, offers){
-  const selectedOffers = offers.map((offer) => /*html*/ `
-  <div
-    class="event__type-item">
-    <input
-      id="event-type-${offer.type}-1"
-      class="event__type-input  visually-hidden"
-      type="radio"
-      name="event-type"
-      value="${offer.type}"
-      ${type === offer.type ? 'checked' : ''}>
-    <label
-      class="event__type-label  event__type-label--${offer.type}"
-      for="event-type-${offer.type}-1">
-      ${offer.type[0].toUpperCase() + offer.type.slice(1)}
-    </label>
-  </div>`);
-
-  return selectedOffers.join('');
-}
-
 
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -27,51 +27,85 @@ export default class PointView {
 
 function createPointViewTemplate(point, destinations, offers) {
 
-  const { basePrice, dateFrom, dateTo, destinationId, isFavorite, offerIds, type} = point;
+  const { basePrice, dateFrom, dateTo, destinationId, isFavorite, offerIds, type } = point;
 
-  return /*html*/`<li class="trip-events__item">
-            <div class="event">
-              <time class="event__date" datetime=${dateFrom}>${formatDate(dateFrom)}</time>
-              <div class="event__type">
-                <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
-              </div>
-              <h3 class="event__title">${type}
-              ${getDestination(destinationId, destinations)}</h3>
-              <div class="event__schedule">
-                <p class="event__time">
-                  <time class="event__start-time"
-                  datetime=${dateFrom}>${formatTime(dateFrom)}</time>
-                  —
-                  <time class="event__end-time"
-                  datetime=${dateTo}">${formatTime(dateTo)}</time>
-                </p>
-                <p class="event__duration">${formatDuration(dateFrom, dateTo)}</p>
-              </div>
-              <p class="event__price">
-                €&nbsp;<span class="event__price-value">${basePrice}</span>
-              </p>
-              <h4 class="visually-hidden">Offers:</h4>
-              <ul class="event__selected-offers">
-                ${getOffersList(point.type, offerIds, offers)}
+  return /*html*/`
+    <li class="trip-events__item">
+      <div class="event">
+        ${getEventDate(dateFrom)}
+        ${getEventIcon(type)}
+        ${getEventTitle(type, destinationId, destinations)}
+        ${getSchedule(dateFrom, dateTo)}
+        ${getPrice(basePrice)}
+        ${getSelectedOffers(point.type, offerIds, offers)}
+        ${getAddFavoriteButton(isFavorite)}
+        ${getOpenEditButton()}
+      </div>
+    </li>`;
+}
 
-              </ul>
-              <button
-                class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}"
-                type="button">
-                <span class="visually-hidden">Add to favorite</span>
-                <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-                  <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
-                </svg>
-              </button>
-              <button class="event__rollup-btn" type="button">
-                <span class="visually-hidden">Open event</span>
-              </button>
-            </div>
-          </li>`;
+function getEventDate(dateFrom){
+  return /*html*/`
+  <time class="event__date" datetime=${dateFrom}>${formatDate(dateFrom)}</time>
+  `;
+}
+
+function getEventIcon(type){
+  return /*html*/`
+  <div class="event__type">
+    <img
+      class="event__type-icon"
+      width="42"
+      height="42"
+      src="img/icons/${type}.png"
+      alt="Event type icon">
+  </div>
+  `;
+}
+
+function getEventTitle(type, destinationId, destinations){
+  return /*html*/`
+  <h3 class="event__title">
+    ${type}
+    ${getDestination(destinationId, destinations)}
+  </h3>
+  `;
 }
 
 function getDestination(destinationId, destinations){
   return destinations.find((destination) => destinationId === destination.id).name;
+}
+
+function getSchedule(dateFrom, dateTo){
+  return /*html*/`
+  <div class="event__schedule">
+    <p class="event__time">
+      <time class="event__start-time"
+      datetime=${dateFrom}>${formatTime(dateFrom)}</time>
+      —
+      <time class="event__end-time"
+      datetime=${dateTo}">${formatTime(dateTo)}</time>
+    </p>
+    <p class="event__duration">${formatDuration(dateFrom, dateTo)}</p>
+  </div>
+  `;
+}
+
+function getPrice(basePrice){
+  return /*html*/`
+  <p class="event__price">
+    €&nbsp;<span class="event__price-value">${basePrice}</span>
+  </p>
+  `;
+}
+
+function getSelectedOffers(type, offerIds, offers){
+  return /*html*/`
+  <h4 class="visually-hidden">Offers:</h4>
+  <ul class="event__selected-offers">
+    ${getOffersList(type, offerIds, offers)}
+  </ul>
+  `;
 }
 
 function getOffersList(pointType, offerIds, offers){
@@ -102,7 +136,25 @@ function getOffersList(pointType, offerIds, offers){
   </li>`);
 
   return selectedOffersStrings.join('');
-
 }
 
+function getAddFavoriteButton(isFavorite){
+  return /*html*/`
+  <button
+    class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}"
+    type="button">
+    <span class="visually-hidden">Add to favorite</span>
+    <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+      <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
+    </svg>
+  </button>
+  `;
+}
 
+function getOpenEditButton(){
+  return /*html*/`
+  <button class="event__rollup-btn" type="button">
+    <span class="visually-hidden">Open event</span>
+  </button>
+  `;
+}
