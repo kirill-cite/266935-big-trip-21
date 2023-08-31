9050fd22d58e185e8a3a0b1ff825155b5006e1e6
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-08-31 17:28:01 +0300

    Добавляет приватные поля и геттеры моделям и BoardPresenter

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -6,34 +6,42 @@ import PointEditView from '../view/edit-point-view.js';
 import { render } from '../framework/render.js';
 
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  pointsListComponent = new PointsListView();
+  #boardContainer = null;
+  #pointsModel = null;
+  #destinationsModel = null;
+  #offersModel = null;
+
+  #boardComponent = new BoardView();
+  #pointsListComponent = new PointsListView();
+
+  #boardPoints = [];
+  #boardDestinations = [];
+  #boardOffers = [];
 
   constructor({boardContainer, pointsModel, destinationsModel, offersModel}) {
-    this.boardContainer = boardContainer;
-    this.pointsModel = pointsModel;
-    this.destinationsModel = destinationsModel;
-    this.offersModel = offersModel;
+    this.#boardContainer = boardContainer;
+    this.#pointsModel = pointsModel;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
   }
 
   init() {
-    this.listPoints = [...this.pointsModel.points];
-    this.listDestinations = [...this.destinationsModel.destinations];
-    this.listOffers = [...this.offersModel.offers];
+    this.#boardPoints = [...this.#pointsModel.points];
+    this.#boardDestinations = [...this.#destinationsModel.destinations];
+    this.#boardOffers = [...this.#offersModel.offers];
 
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.element);
-    render(this.pointsListComponent, this.boardComponent.element);
-    render(new PointEditView({point : this.listPoints[0],
-      destinations: this.listDestinations,
-      offers: this.listOffers}), this.pointsListComponent.element);
+    render(this.#boardComponent, this.#boardContainer);
+    render(new SortView(), this.#boardComponent.element);
+    render(this.#pointsListComponent, this.#boardComponent.element);
+    render(new PointEditView({point : this.#boardPoints[0],
+      destinations: this.#boardDestinations,
+      offers: this.#boardOffers}), this.#pointsListComponent.element);
 
-    for (let i = 1; i < this.listPoints.length; i++) {
-      render(new PointView({point : this.listPoints[i],
-        destinations: this.listDestinations,
-        offers: this.listOffers}),
-      this.pointsListComponent.element);
+    for (let i = 1; i < this.#boardPoints.length; i++) {
+      render(new PointView({point : this.#boardPoints[i],
+        destinations: this.#boardDestinations,
+        offers: this.#boardOffers}),
+      this.#pointsListComponent.element);
     }
-
   }
 }
