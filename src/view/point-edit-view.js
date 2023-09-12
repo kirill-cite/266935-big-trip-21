740297a39744c9f8562068ba0da8337f9aac924d
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-09-12 18:55:35 +0300

    Устраняет ошибку

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -37,7 +37,7 @@ export default class PointEditView extends AbstractView {
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
-    this.#handleFormSubmit();
+    this.#handleFormSubmit(this.#point);
   };
 
   #rollUpHandler = (evt) => {
