67f5e4c1d781986f8f8d5522308378b016c83220
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-09-18 17:32:43 +0300

    Реализовали в EditorView отправку события delete

--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -347,8 +347,10 @@ class EditorView extends View {
    * @param {Event} event
    */
   onReset(event){
+    const { id } = this.state;
+
     event.preventDefault();
-    this.dispatch('close');
+    this.dispatch(id === 'draft' ? 'close' : 'delete');
   }
 }
 
